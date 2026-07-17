864c4c115bc2996782d27cfc801ba2a020554b3b
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2026-07-17 09:39:31 +0700

    optimize text node

--- component-service/src/main/java/com/simple/component/service/CoroutinesExt.kt
+++ component-service/src/main/java/com/simple/component/service/CoroutinesExt.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 fun <T> Flow<T>.launchCollect(lifecycleOwner: LifecycleOwner, action: suspend (T) -> Unit) = lifecycleOwner.lifecycleScope.launch {
     collect { data ->
@@ -12,7 +14,7 @@ fun <T> Flow<T>.launchCollect(lifecycleOwner: LifecycleOwner, action: suspend (T
     }
 }
 
-fun <T> Flow<T>.launchCollect(scope: CoroutineScope, action: suspend (T) -> Unit) = scope.launch {
+fun <T> Flow<T>.launchCollect(scope: CoroutineScope, context: CoroutineContext = EmptyCoroutineContext, action: suspend (T) -> Unit) = scope.launch(context = context) {
     collect { data ->
         action(data)
     }
